092a72e943e70a95c9cf6b314242bab5f5571f37
arunvijo <arunvijo2004@gmail.com>
2025-11-03 18:01:19 +0530

    feat: auth0 credentials updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,43 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-import Home from "./pages/Home";
-import AdminDashboard from "./pages/AdminDashboard";
+import Home from "./pages/Home.jsx";
+import AdminDashboard from "./pages/AdminDashboard.jsx";
 
-import CreateParticipant from "./pages/Participant/CreateParticipant";
-import EditParticipant from "./pages/Participant/EditParticipant";
-import ShowParticipant from "./pages/Participant/ShowParticipant";
-import DeleteParticipant from "./pages/Participant/DeleteParticipant";
+import CreateParticipant from "./pages/Participant/CreateParticipant.jsx";
+import EditParticipant from "./pages/Participant/EditParticipant.jsx";
+import ShowParticipant from "./pages/Participant/ShowParticipant.jsx";
+import DeleteParticipant from "./pages/Participant/DeleteParticipant.jsx";
 
-import CreateEvent from "./pages/Event/CreateEvent";
-import EditEvent from "./pages/Event/EditEvent";
-import ShowEvent from "./pages/Event/ShowEvent";
-import DeleteEvent from "./pages/Event/DeleteEvent";
+import CreateEvent from "./pages/Event/CreateEvent.jsx";
+import EditEvent from "./pages/Event/EditEvent.jsx";
+import ShowEvent from "./pages/Event/ShowEvent.jsx";
+import DeleteEvent from "./pages/Event/DeleteEvent.jsx";
 
-import AdminCreateRegistration from "./pages/Registration/CreateRegistration";
-import EditRegistration from "./pages/Registration/EditRegistration";
-import ShowRegistration from "./pages/Registration/ShowRegistration";
-import DeleteRegistration from "./pages/Registration/DeleteRegistration";
+import CreateRegistration from "./pages/Registration/CreateRegistration.jsx";
+import EditRegistration from "./pages/Registration/EditRegistration.jsx";
+import ShowRegistration from "./pages/Registration/ShowRegistration.jsx";
+import DeleteRegistration from "./pages/Registration/DeleteRegistration.jsx";
 
-import CreateScore from "./pages/Score/CreateScore";
-import EditScore from "./pages/Score/EditScore";
-import ShowScore from "./pages/Score/ShowScore";
-import DeleteScore from "./pages/Score/DeleteScore";
+import CreateScore from "./pages/Score/CreateScore.jsx";
+import EditScore from "./pages/Score/EditScore.jsx";
+import ShowScore from "./pages/Score/ShowScore.jsx";
+import DeleteScore from "./pages/Score/DeleteScore.jsx";
 
-import CaptainDashboard from "./pages/Captain/CaptainDashboard";
-import EventView from "./pages/Captain/EventView";
-import CreateRegistration from "./pages/Captain/CreateRegistration";
+import CaptainDashboard from "./pages/Captain/CaptainDashboard.jsx";
+import EventView from "./pages/Captain/EventView.jsx";
 
-import "./index.css"
-import Scoreboard from "./pages/Scoreboard";
-import AdminEventView from "./components/admin/AdminEventView";
-import Events from "./pages/Events";
-import EventPage from "./pages/EventPage";
+import "./index.css";
+import Scoreboard from "./pages/Scoreboard.jsx";
+import AdminEventView from "./components/admin/AdminEventView.jsx";
+import Events from "./pages/Events.jsx";
+import EventPage from "./pages/EventPage.jsx";
 
-const Participant = () => {
+const App = () => { // Changed component name from Participant to App
   return (
     <Auth0Provider
-      domain="dev-6edntfe6rlcf5tb7.us.auth0.com"
-      clientId="PVkwq4rtF10qzeLYudDPQnISZycU7kRS"
+      domain="dev-c7fiqa1rj3dt5eb0.us.auth0.com" // <-- PASTE YOUR NEW DOMAIN
+      clientId="i9L3qP0AKqqPEufUmyjUlm8xMLAxaE7r" // <-- PASTE YOUR NEW CLIENT ID
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
@@ -56,28 +55,28 @@ const Participant = () => {
         <Route path="/participant/details/:id" element={<ShowParticipant />} />
         <Route path="/participant/edit/:id" element={<EditParticipant />} />
         <Route path="/participant/delete/:id" element={<DeleteParticipant />} />
-        
+
         <Route path="/event/create" element={<CreateEvent />} />
         <Route path="/event/details/:id" element={<ShowEvent />} />
         <Route path="/event/edit/:id" element={<EditEvent />} />
         <Route path="/event/delete/:id" element={<DeleteEvent />} />
-        
-        <Route path="/registration/create" element={<AdminCreateRegistration />} />
+
+        <Route path="/registration/create" element={<CreateRegistration />} />
         <Route path="/registration/details/:id" element={<ShowRegistration />} />
         <Route path="/registration/edit/:id" element={<EditRegistration />} />
         <Route path="/registration/delete/:id" element={<DeleteRegistration />} />
-        
+
         <Route path="/score/create" element={<CreateScore />} />
         <Route path="/score/details/:id" element={<ShowScore />} />
         <Route path="/score/edit/:id" element={<EditScore />} />
         <Route path="/score/delete/:id" element={<DeleteScore />} />
-        
+
         <Route path="/captain" element={<CaptainDashboard />} />
         <Route path="/captain/event/view/:id" element={<EventView />} />
-        <Route path="/captain/registration/create" element={<CreateRegistration />} />
       </Routes>
     </Auth0Provider>
   );
 };
 
-export default Participant;
+export default App; // Changed default export
+
